27b91d0f74f5cd0e433012cbe68ae50b191578b3
coreyoneumbrellarecords-glitch <coreyoneumbrellarecords@gmail.com>
2026-09-17 03:35:42 +0000

    Enable message content intent

--- main.py
+++ main.py
@@ -59,6 +59,7 @@ init_db()
 class StorytimeBot(commands.Bot):
     def __init__(self):
         intents = discord.Intents.default()
+        intents.message_content = True
         super().__init__(command_prefix="!", intents=intents)
 
     async def setup_hook(self):
